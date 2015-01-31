5b2ce7c637204419a25ea8d0fc02d611de9c4ea7
Aishi Jiang <jiangaishi@gmail.com>
2015-01-31 00:30:22 -0500

    init

---
+++ recallme.py
@@ -0,0 +1,30 @@
+# all the imports
+import sqlite3
+import twilio.twiml
+from flask import Flask, request, session, g, redirect, url_for, \
+abort, render_template, flash
+from contextlib import closing
+
+app = Flask(__name__)
+
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+	with closing(connect_db()) as db:
+		with app.open_resource('schema.sql', mode='r') as f:
+			db.cursor().executescript(f.read())
+	db.commit()
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+ 
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
+ 
+if __name__ == '__main__':
+	app.run(debug=True)
+ 
+
