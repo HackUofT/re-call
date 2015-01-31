5973f6cf1dbd8ccd7603b04392e94177ff415e67
Aishi Jiang <jiangaishi@gmail.com>
2015-01-31 03:34:19 -0500

    add voice call

--- recallme.py
+++ recallme.py
@@ -38,24 +38,7 @@ def voice_reminder():
     	from_="+15873169685")
 	print call.sid
 
-@app.route("/record", methods=['GET', 'POST'])
-def record():
-	 resp = twilio.twiml.Response()
-     resp.say("Record your monkey howl after the tone.")
-     resp.record(maxLength="30", action="/handle-recording")
-     return str(resp)
 
-@app.route("/handle-recording", methods=['GET', 'POST'])
-def handle_recording():
-    """Play back the caller's recording."""
- 
-    recording_url = request.values.get("RecordingUrl", None)
- 
-    resp = twilio.twiml.Response()
-    resp.say("Thanks for howling... take a listen to what you howled.")
-    resp.play(recording_url)
-    resp.say("Goodbye.")
-    return str(resp)
 
 if __name__ == '__main__':
 	app.run(debug=True)
