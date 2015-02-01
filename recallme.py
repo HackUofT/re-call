80d5e6cc6363b25aa278f7b242ff193147186846
ruthgrace <ruthgracewong@gmail.com>
2015-02-01 03:48:51 -0500

    broken app

--- recallme.py
+++ recallme.py
@@ -54,8 +54,20 @@ def text_reminder():
     resp.message("Go to the fucking gymmmmmmmm")
     resp.say("Record your message to yourself after the tone.")
     resp.record(maxLength="15", action="/handle-recording")
-    return str(resp)
 
+    # show_entries from http://flask.pocoo.org/docs/0.10/tutorial/views/#tutorial-views
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
 
 
  
