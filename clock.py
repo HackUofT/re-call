b0522674b59139a32bfb3877b875fbf5e1b3c40b
ruthgrace <ruthgracewong@gmail.com>
2015-02-01 09:22:44 -0500

    tried to fix indenting in clock.py

--- clock.py
+++ clock.py
@@ -9,9 +9,9 @@ sched = BlockingScheduler()
 def new_reminder(reminderDate):
 	@sched.add_job(my_job, 'date', run_date=reminderDate, args=['text'])
 #    @sched.interval_schedule(seconds=2)
-    def job_function():
-    	queue = Queue(connection=conn)
-    	return()
+	def job_function():
+		queue = Queue(connection=conn)
+		return()
 #        print "Hello World"
 
 
